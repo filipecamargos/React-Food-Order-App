940f04cf425365e5ec150a0328576ece34003616
filipecamargos <filipe.atrabalho@gmail.com>
2021-05-03 21:08:43 -0600

    Banner header image added

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,16 +1,17 @@
 import { Fragment } from 'react';
 
 import bannerImage from '../../assets/bannerFood.jpg';
+import classes from './Header.module.css';
 
 const Header = (props) => {
   return (
     <Fragment>
-      <header>
+      <header className={classes.header}>
           <h1>Meal Order App</h1>
           <button>Cart</button>
       </header>
-      <div>
-          <img src={bannerImage} />
+      <div className={classes['main-image']}>
+          <img src={bannerImage} alt='Table of food'/>
       </div>
     </Fragment>
   );
