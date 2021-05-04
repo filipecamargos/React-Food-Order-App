56f568e2becab523aaecad7787825affbcbc4275
filipecamargos <filipe.atrabalho@gmail.com>
2021-05-03 21:36:45 -0600

    Meals added

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,21 @@
+import classes from './MealsSummary.module.css';
+
+const MealSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Meal For you!</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae, ab a!
+        Id, possimus voluptatum beatae fugit similique tenetur tempore quis qui
+        itaque quae ipsa.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio aspernatur
+        accusamus quaerat illo cum ullam ad eaque sapiente blanditiis dolorem
+        quibusdam doloremque.
+      </p>
+    </section>
+  );
+};
+
+export default MealSummary;
